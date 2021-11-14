45236abf6ffcd10dd4262a1666c6848b5b5439d0
julianstreibel <julian.streibel@freenet.de>
2021-11-14 10:28:40 +0100

    client

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,5 +1,51 @@
+import React, { useState } from 'react'
+
 export const Main = () => {
+
+    let [returnMessages, setReturnMessages] = useState([])
+
+    const doSomeThing = () => {
+        const msg = "Done :)"
+        addMassage(msg)
+    }
+
+    const addMassage = (msg) => {
+        setReturnMessages([...returnMessages, `\n ${msg}`])
+
+    }
+
+    const buttenStyle = {
+        backgroundColor: 'grey',
+        width: '150px',
+        height: '50px',
+        lineHeight: '50px',
+        textAlign: 'center',
+        cursor: 'pointer',
+        marginTop: '20px'
+    }
+
+    const buttonContainerStyle = {
+        position: 'absolute',
+        top: '10vh',
+        left: 'calc(50vw - 75px)'
+    }
+
     return (
-        <div> HI</div>
+        <>
+            <div
+                style={buttonContainerStyle}
+            >
+                <div
+                    style={buttenStyle}
+                    onClick={doSomeThing}
+                > do something</div>
+
+                <div
+                    style={buttenStyle}
+                    onClick={doSomeThing}
+                > do something</div>
+            </div>
+            {returnMessages.map(msg => <div>{msg}</div>)}
+        </>
     )
 }
