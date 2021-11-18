cb7cdc26f93e90279ae262fefb949561caec8ea0
Max <maxmeuer@me.com>
2021-11-18 18:32:56 -0500

    added Price Feed

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -1,10 +1,14 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.4.22 <0.8.0;
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
 
 contract Campaign {
     address payable public receipient;
     uint256 public expiration_date;
+    AggregatorV3Interface internal priceFeed;
+
     // uint public goal;
 
     // error AuctionAlreadyEnded();
@@ -12,9 +16,10 @@ contract Campaign {
     event DonationSend(address donor, uint256 amount);
     event DonationEnded(address receipient, uint256 amount);
 
-    constructor(uint256 duration, address payable receipient_adress) public {
+    constructor(uint256 duration, address payable receipient_adress, uint256 goal) public {
         receipient = receipient_adress;
-        // goal = goal;
+        goal = goal;
+        priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
         expiration_date = block.timestamp + duration;
     }
 
@@ -24,7 +29,20 @@ contract Campaign {
         emit DonationSend(msg.sender, msg.value);
     }  
 
-    function payout() external {
+    function getLatestPrice() public view returns (int) {
+        (
+            uint80 roundID, 
+            int price,
+            uint startedAt,
+            uint timeStamp,
+            uint80 answeredInRound
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function payout() public {
+        if(getLatestPrice() < goal)
+            revert("Donation Campaign has not reached its end goal");
         if (block.timestamp < expiration_date)
             revert("Donation Campaign has not Ended Yet");
         emit DonationEnded(receipient, address(this).balance);
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,5 +3,5 @@ var Campaign = artifacts.require("./Campaign.sol");
 module.exports = function(deployer) {
     let duration = 180
     let receipient_adress = '0x605CEF716cC3cc9B3705FB971c68C621747635cA'
-  deployer.deploy(Campaign, duration, receipient_adress);
+  deployer.deploy(Campaign, duration, receipient_adress, goal);
 };
